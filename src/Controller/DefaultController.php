33496ee56b1bdcf14b1d088b973a794256edeed5
julien <julien.clairo@iia-formation.fr>
2023-04-26 11:58:31 +0200

    Gestion de la page d'accueil fabrication du menu Category

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_hompage')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository, TrickRepository $trickRepository): Response
     {
+        $categories= $categoryRepository->findAll();
+        $tricks=$trickRepository->findAll();
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'categories'=>$categories,
+            'tricks'=>$tricks
         ]);
     }
 }
